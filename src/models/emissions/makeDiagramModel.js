6f68db4ad7bccc68fed00fb44081775117d39643
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2017-01-11 17:49:27 +0000

    Default completion to max value if end isn't passed on makeDiagramModel

--- src/models/emissions/makeDiagramModel.js
+++ src/models/emissions/makeDiagramModel.js
@@ -1,6 +1,15 @@
-const makeDiagramModel = (emissions, completion) => ({
-  emissions,
-  completion
-})
+const selectValue = obj => obj.x
+
+const makeDiagramModel = (emissions, end) => {
+  let completion = end
+  if (!completion) {
+    completion = Math.max.apply(null, emissions.map(selectValue))
+  }
+
+  return {
+    emissions,
+    completion
+  }
+}
 
 export default makeDiagramModel
