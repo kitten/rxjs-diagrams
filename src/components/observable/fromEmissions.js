297f7113b578b1014c7ef65c78a812a4f48ae9a8
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2017-01-11 02:21:08 +0000

    Remove minimum adjustment from fromEmissions helper

--- src/components/observable/fromEmissions.js
+++ src/components/observable/fromEmissions.js
@@ -4,7 +4,7 @@ import { isEmissionsArr } from '../../utils/isEmissionsArr'
 
 const selectValue = obj => obj.x
 
-function fromEmissions(arr, end, completion) {
+function fromEmissions(arr, range, completion) {
   if (!isEmissionsArr(arr)) {
     throw new Error([
       'Expected each value in `emissions` to be an emission',
@@ -13,23 +13,20 @@ function fromEmissions(arr, end, completion) {
   }
 
   const min = Math.min.apply(null, arr.map(selectValue))
-  const max = typeof end === 'number' ? end : Math.max.apply(null, arr.map(selectValue))
-  const range = max - min
-
-  const minFactor = min / range
+  const max = typeof range === 'number' ? range : Math.max.apply(null, arr.map(selectValue))
 
   const emissions = arr
     .filter(({ x }) => x <= max)
     .sort((a, b) => a.x - b.x)
     .map(({ x, ...rest }) => ({
       ...rest,
-      x: x / range - minFactor
+      x: x / max
     }))
 
   return props => (
     <ObservableView
       {...props}
-      completion={completion ? Math.min(completion / range, 1) : 0}
+      completion={completion ? Math.min(completion / max, 1) : 0}
       emissions={emissions}
     />
   )
