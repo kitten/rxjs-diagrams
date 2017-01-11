a5ff2243aea9a8540f494c277f74dd2d9d97ed01
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2017-01-11 03:58:10 +0000

    Add combineLatest example to storybook

--- src/models/emissions/index.stories.js
+++ src/models/emissions/index.stories.js
@@ -4,6 +4,7 @@ import { distinctUntilChanged } from 'rxjs/operator/distinctUntilChanged'
 import { map } from 'rxjs/operator/map'
 import { first } from 'rxjs/operator/first'
 import { delay } from 'rxjs/operator/delay'
+import { combineLatest } from 'rxjs/observable/combineLatest'
 
 import ObservableRenderer from '../../components/ObservableRenderer'
 import { transformEmissions } from './index'
@@ -127,6 +128,49 @@ storiesOf('Emissions', module)
     return (
       <div>
         <Input/>
+
+        <ObservableRenderer
+          source={output}
+          transform={({ emissions, completion }) => {
+            const View = fromEmissions(emissions, width, completion)
+            return <View/>
+          }}
+        />
+      </div>
+    )
+  })
+  .add('.combineLatest((x, y) => `${x}${y}`)', () => {
+    const width = 80
+    const inputA = [
+      { x: 5, d: 1 },
+      { x: 20, d: 2 },
+      { x: 35, d: 3 },
+      { x: 60, d: 4 },
+      { x: 70, d: 5 }
+    ]
+
+    const inputB = [
+      { x: 5, d: 'A' },
+      { x: 28, d: 'B' },
+      { x: 35, d: 'C' },
+      { x: 45, d: 'D' },
+      { x: 70, d: 'E' }
+    ]
+
+    const output = transformEmissions(
+      (a, b, scheduler) => combineLatest(a, b, (x, y) => `${x}${y}`, scheduler),
+      width,
+      inputA,
+      inputB
+    )
+
+    const InputA = fromEmissions(inputA, width, width)
+    const InputB = fromEmissions(inputB, width, width)
+
+    return (
+      <div>
+        <InputA/>
+        <InputB/>
 
         <ObservableRenderer
           source={output}
